8317eb2a0721e97154bca80e3d53d49a20d26e52
MGSimard <mgsimard.dev@gmail.com>
2025-03-03 00:26:58 -0500

    easports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,7 +12,10 @@ function App() {
         <li>Outlet as index.tsx</li>
         <li>autoCodeSplitting true</li>
         <li>RouterProvider defaultPendingMinMs untouched (500 default)</li>
-        <li>Setting defaultPendingMinMs to 0 would render in ~250-300ms</li>
+        <li>
+          Setting defaultPendingMinMs to 0 would render in ~250-300ms (NOTE: This is apparently React 19's fault, which
+          adds an artificial 300ms delay)
+        </li>
         <li>Basically, disable TanStackRouteVite autoCodeSplitting in vite.config.js if you don't need it</li>
         <li>
           In a perfect world some turbogenius makes it so the immediately requested route on load isn't lazy-loaded at
